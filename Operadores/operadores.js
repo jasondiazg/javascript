bbf82e1ad01ccd10e99b39fdcc739313bb9ee2f8
Jason Diaz G <jasondiazg@gmail.com>
2020-07-03 12:22:28 -0600

    ADDED: more examples in operators demo.

--- Operadores/operadores.js
+++ Operadores/operadores.js
@@ -9,9 +9,9 @@ function evaluarAND() {
 }
 
 function evaluarOR() {
-    console.log("true || true es " + (true  || true));
+    console.log("true || true es " + (true || true));
     console.log("false || true es " + (false || true));
-    console.log("true || false es " + (true  || false));
+    console.log("true || false es " + (true || false));
     console.log("false || (3 == 4) es " + (false || (3 == 4)));
     console.log("\"Cat\" || \"Dog\" es " + ("Cat" || "Dog"));
     console.log("false || \"Cat\" es " + (false || "Cat"));
@@ -29,10 +29,27 @@ function concatenar() {
 }
 
 function sumar() {
-    alert("numero + numeroEnCadena es " + (2 + "5") + " typeof " + typeof(2 + "5"));
+    let numero = "35";
+    alert("numero + numeroEnCadena es " + (2 + (isNaN(numero) ? 1 : parseInt(numero))) + " typeof " + typeof (2 + parseInt(numero)));
 }
 
 function operadorComa() {
+    let fruits = ["apple", "grape"];
+    let numbers = [1, 8, 23];
+    let students = [
+        {
+            name: "Jefree",
+            age: 22
+        },
+        {
+            name: "Urias",
+            age: 25
+        }
+    ];
+
+    console.log(fruits[0]);
+    console.log(numbers[2]);
+
     let a = [["Piña", "Naranja", "Manzana"], ["Limón", "Papa", "Tomate"], ["Rojo", "Azul", "Verde"]];
     for (var i = 0, j = 2; i <= 2 && j >= 0; i++, j--)
         console.log("a[" + i + "][" + j + "]= " + a[i][j]);
@@ -41,12 +58,18 @@ function operadorComa() {
 function operadorTernario() {
     let edad = parseInt(document.getElementById("edad").value);
 
-    alert("Usted es " + (edad >= 18 ? "mayor" : "menor") + " de edad.");
-    document.getElementById("edad").value = "";
+    if (isNaN(edad)) {
+        alert("Cuidado, usted no ingresó un valor númerico")
+    } else {
+        alert("Usted es " + (edad >= 18 ? "mayor" : "menor") + " de edad.");
+        document.getElementById("edad").value = "";
+    }
 }
 
 function unarioDelete() {
-    let objeto = { propiedad: "valor" }
+    let objeto = {
+        propiedad: "valor"
+    }
     console.log("Objeto: " + objeto);
     console.log("Propiedad dentro de objeto: " + objeto.propiedad);
     console.log("Eliminando propiedad dentro de objeto");
@@ -86,9 +109,9 @@ function unarioDelete() {
     }
 }
 
-function mostrarArregloEnConsola(arreglo) {
-    for (var i=0; i<=arreglo.length - 1; i++) {
-        console.log(arreglo[i]);
+function mostrarArregloEnConsola(array) {
+    for (var i = 0; i <= array.length - 1; i++) {
+        console.log(array[i]);
     }
 }
 
@@ -97,8 +120,8 @@ function unarioTypeOf() {
     let b = "2";
     let c = { d: "e" };
     let d = new Date();
-    let e = function() { };
-    let f = true
+    let e = function () { };
+    let f = true;
 
     console.log(typeof a);
     console.log(typeof b);
@@ -109,34 +132,64 @@ function unarioTypeOf() {
     console.log(typeof g);
     console.log(typeof null);
     console.log(typeof undefined);
+
+    let numero = "32", numero2 = 5;
+    let suma = 0;
+
+    if (typeof numero === 'string') {
+        console.log("Convirtiendo el valor a número...");
+        numero = parseInt(numero);
+        suma = numero + numero2;
+    } else {
+        suma = numero + numero2;
+    }
+
+    console.log(suma);
+
 }
 
 function relacionalIn() {
+    // EVALUACIÓN VALOR LÓGICO NOT
+    let nombre = "Jefree";
+    let nombre2 = "Otro valor";
+
+    if (nombre === "Jefree") {
+        console.log("El nombre si es Jefree.");
+    }
+
+    if (!(nombre2 === "Jefree")) {
+        console.log("El nombre no es Jefree");
+    }
+
     // Arrays
     var arboles = new Array("secoya", "laurel", "cedro", "roble", "arce");
     if (0 in arboles) {
         console.log("Si se encuentra la posición 0 en el arreglo de árboles... árbol: " + arboles[0]);
     }
+
     if (!(6 in arboles)) {
         console.log("No se encuentra la posición 6 en el arreglo de árboles... " + arboles[6]);
     }
     if (!("laurel" in arboles)) { // devuelve false (Se debe especificar el número de índice, no el valor contenido en ese índice)
         console.log("No se puede acceder al contenido de una variable con in, si no es por medio del índice");
     }
-    
+
     // String creados
     var miCadena = new String("coral");
     if ("length" in miCadena) { // devuelve true (length es una propiedad del String)
-        console.log("Length si es una propiedad de un string por lo tanto si existe");
+        console.log("Length si es una propiedad de un string por lo tanto si existe" + " el valor de lenght es: " + miCadena.length);
     }
 
     // Objetos creados
-    var carro = {marca: "Honda", modelo: "Civic", fecha: 2008};
+    var carro = { marca: "Toyota", modelo: "Hilux", fecha: 2020 };
     if ("marca" in carro) {
         console.log(carro.marca);
     }
     if ("modelo" in carro) {
         console.log(carro.modelo);
+    }
+    if (!("otraPropiedad" in carro)) {
+        console.log("No existe otraPropiedad dentro de mi objeto carro");
     }
 }
 
@@ -173,15 +226,15 @@ function evaluarInstanceOf(parametro) {
 }
 
 function calcularSubtotal(index) {
-    let cantidad = parseInt(document.getElementById("cantidad"+index).value);
-    let costo = parseInt(document.getElementById("costo"+index).value);
+    let cantidad = parseInt(document.getElementById("cantidad" + index).value);
+    let costo = parseInt(document.getElementById("costo" + index).value);
     if (cantidad && costo)
-        return document.getElementById("subtotal"+index).value = cantidad * costo;
+        return document.getElementById("subtotal" + index).value = cantidad * costo;
 }
 
 function calcularTotal() {
     let total = 0;
-    for (let i=1; i<=2; i++) {
+    for (let i = 1; i <= 2; i++) {
         total += calcularSubtotal(i);
     }
     alert("El total de la compra es " + total);
